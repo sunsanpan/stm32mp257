4de5188714f62fa6141c2f80828264c1ebcffe9a
Sunny Pandey <sunnykumar.p76@gmail.com>
2026-02-13 22:54:54 +0530

    Delete cFS directory

--- cFS/cfe/cmake/Makefile-multitarget/eds-test.mk
+++
@@ -1,1 +0,0 @@
-include native-test.mk
--- cFS/cfe/modules/config/cmake/create_cfe_platform_cfg.cmake
+++
@@ -1,5 +0,0 @@
-
-file(READ ${LIST_FILE} PLATCFG_LIST)
-file(READ ${CONTENT_FILE} PLATCFG_CONTENT)
-
-configure_file(cfe_platform_cfg.c.in ${OUTPUT_FILE} @ONLY)
